99821124d3cf4b6dbe99bbb30d12b245917a7f78
BhargavaBR <bhargavravi143@gmail.com>
2023-05-07 17:19:51 +0530

    Updated application.yaml and added basic controllers to application
    Signed-off-by: BhargavaBR <bhargavravi143@gmail.com>

---
+++ src/main/java/com/Project/BlogAPI/Users/UserController.java
@@ -0,0 +1,9 @@
+package com.Project.BlogAPI.Users;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+}
